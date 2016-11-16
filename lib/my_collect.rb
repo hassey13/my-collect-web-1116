ef2fbc53c42e8bbf6b7f4c59c8132acd22e2b65b
Eric Hasselbring <ehasselbring@gmail.com>
2016-11-16 20:14:23 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,10 @@
-
-
+def my_collect(array) # put argument(s) here
+  # code here
+  local_array = []
+  counter = 0
+  while counter < array.length
+      local_array <<  yield(array[counter])
+      counter += 1
+  end
+  return local_array
+end
